5a939ed8618b9c3dce74f5e70724f1fa37e4333a
ADBond <48208438+ADBond@users.noreply.github.com>
2024-09-11 17:44:54 +0100

    some simple tests for running without error

---
+++ tests/test_basic_functionality.py
@@ -0,0 +1,32 @@
+from splink import Linker, block_on, splink_datasets
+
+
+def test_make_linker(db_api, fake_1000_settings):
+    df = splink_datasets.fake_1000
+    Linker(df, fake_1000_settings, db_api)
+
+def test_train_u(db_api, fake_1000_settings):
+    df = splink_datasets.fake_1000
+    linker = Linker(df, fake_1000_settings, db_api)
+    linker.training.estimate_u_using_random_sampling(max_pairs=3e4)
+
+def test_train_lambda(db_api, fake_1000_settings):
+    df = splink_datasets.fake_1000
+    linker = Linker(df, fake_1000_settings, db_api)
+    linker.training.estimate_probability_two_random_records_match(
+        [
+            block_on("dob"),
+            block_on("first_name", "surname")
+        ],
+        recall=0.8
+    )
+
+def test_em_training(db_api, fake_1000_settings):
+    df = splink_datasets.fake_1000
+    linker = Linker(df, fake_1000_settings, db_api)
+    linker.training.estimate_parameters_using_expectation_maximisation(
+        block_on("dob"),
+    )
+    linker.training.estimate_parameters_using_expectation_maximisation(
+        block_on("first_name", "surname"),
+    )
