7dd59176800528e4080fe0688fad992f28467591
ADBond <48208438+ADBond@users.noreply.github.com>
2024-09-12 09:22:11 +0100

    test cumulative_comparisons_to_be_scored_from_blocking_rules_chart

--- tests/test_basic_functionality.py
+++ tests/test_basic_functionality.py
@@ -1,4 +1,7 @@
 from splink import Linker, block_on
+from splink.blocking_analysis import (
+    cumulative_comparisons_to_be_scored_from_blocking_rules_chart,
+)
 
 
 def test_make_linker(api_info, fake_1000_factory, fake_1000_settings):
@@ -50,4 +53,17 @@ def test_clustering(api_info, fake_1000_factory, fake_1000_settings):
     linker.clustering.cluster_pairwise_predictions_at_threshold(
         df_predict,
         threshold_match_probability=0.8,
+    )
+
+def test_cumulative_comparisons(api_info, fake_1000_factory, fake_1000_settings):
+    db_api = api_info["db_api"]
+    df = fake_1000_factory(api_info["version"])
+
+    blocking_rules = fake_1000_settings.blocking_rules_to_generate_predictions
+
+    cumulative_comparisons_to_be_scored_from_blocking_rules_chart(
+        table_or_tables=df,
+        blocking_rules=blocking_rules,
+        db_api=db_api,
+        link_type="dedupe_only",
     )
