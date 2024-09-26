a532a3a66f69953cf40ab3116833c0de71f09668
ADBond <48208438+ADBond@users.noreply.github.com>
2024-09-26 11:11:18 +0100

    wrap top-level importing with more helpful error
    useful message if users try to import ChDBAPI without installing chdb extras

--- splinkclickhouse/__init__.py
+++ splinkclickhouse/__init__.py
@@ -1,6 +1,25 @@
-from .chdb.database_api import ChDBAPI
+from typing import Any
+
 from .clickhouse.database_api import ClickhouseAPI
 
 __version__ = "0.2.5"
+
+
+# Use getarr to make the error appear at the point of use
+def __getattr__(name: str) -> Any:
+    try:
+        if name == "ChDBAPI":
+            from .chdb.database_api import ChDBAPI
+
+            return ChDBAPI
+    except ImportError as err:
+        if name == "ChDBAPI":
+            raise ImportError(
+                f"'{name}' cannot be imported because its dependencies are not "
+                "installed. To get these please install with the 'chdb' extras: "
+                "`pip install splinkclickhouse[chdb]`."
+            ) from err
+    raise ImportError(f"cannot import name '{name}' from splinkclickhouse") from None
+
 
 __all__ = ["ChDBAPI", "ClickhouseAPI"]
