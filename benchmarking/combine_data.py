3dd27607a1fda97d6a9339b7b7126030e80a798c
ADBond <48208438+ADBond@users.noreply.github.com>
2024-10-31 09:51:09 +0000

    make combined data into crude stacked bar

--- benchmarking/combine_data.py
+++ benchmarking/combine_data.py
@@ -1,5 +1,8 @@
 import json
 import os
+from copy import deepcopy
+
+import altair as alt
 
 records = []
 for _root, _dirs, files in os.walk("benchmarking/output"):
@@ -13,3 +16,17 @@ for _root, _dirs, files in os.walk("benchmarking/output"):
 
 with open("benchmarking/output/combined_run_data.json", "w+") as f:
     json.dump(records, f, indent=4)
+
+with open("benchmarking/utils/stacked_bar_spec.json", "r") as f:
+    graph_dict = json.load(f)
+
+graph_dict["data"]["values"] = records
+
+for data_choice in ("fake_1000",):
+    data_graph_dict = deepcopy(graph_dict)
+    graph_dict["transform"][0]["filter"] = graph_dict["transform"][0]["filter"].replace(
+        "__data_choice__", data_choice
+    )
+
+    chart = alt.Chart.from_dict(graph_dict)
+    chart.save(f"benchmarking/output/chart_{data_choice}.html")
